a12e6c0fdd8f65fc1d6bc1ee2dfd23dd6b1584b3
Ubuntu <ubuntu@ip-172-31-37-226.us-west-2.compute.internal>
2023-10-17 13:10:43 +0000

    update

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 # UDACITY CLOUD DEVOPS ENGINEERING: CAPSTONE PROJECT
 
-## Introduction
-
 ## Project Rubrics:
-✔️ Create Github repository with project code. \
-✔️ Use Image repository to store Docker images. \
+✔️ Create Github repository to store my work \
+✔️ Use Image repository to store Docker images \
 ✔️ Build a Docker container in a pipeline. \
 ✔️ Build and push Docker Image to Docker hub. \
 ✔️ Execute linting step in executing pipeline. \
 ✔️ The Docker container is deployed to a Kubernetes cluster. \
 ✔️ Use Blue/Green Deployment.
  
-## Project Deployment Steps:
-1. 
-2. 
-3. 
-4.
+## Project Coding Environment Setting:
+I used AWS Cloud9 to code and control project version, CircleCI to automate the process:
+- Step 1: Set up CircleCI environment parameters: AWS credentials and Docker hub credentials.
+- Step 2: In AWS IAM, create new user and a new key-pair in region US-WEST-2 (EKS cluster can't be created with another region).
+- Step 3: In AWS Cloud9, when choosing EC2 instance (newly-created), choose type **t3.small** for sufficient memory.
+- Step 4: Setup Python virtual environment before working.
+
+## Project Running Steps:
+- Step 1: Configure AWS access credentials (newly-created user)
+- Step 2: Check if eksctl installed, if not, follow this link to install: https://eksctl.io/installation/
+- Step 3: Check if kubectl installed, if not, install with following commands: 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+- Step 4: Run `eksctl create cluster --config-file=./infra/create_eks_cluster.yml`
+- Step 5: CircleCI running
+- Step 6: Check Load Balancer in EC2 to open flask app
--- bin/assume_role.sh
+++ bin/assume_role.sh
@@ -1,49 +1,36 @@
 #!/bin/bash
 # Using to export AWS credentials as environment variables
 
-# Usage: source assume_role.sh
+# Unset any variables HTTP_PROXY, HTTPS_PROXY, NO_PROXY, http_proxy, https_proxy, no_proxy.
 unset http_proxy https_proxy no_proxy
-# Unset any existing HTTP_PROXY, HTTPS_PROXY, and NO_PROXY variables (if set).
 unset HTTP_PROXY HTTPS_PROXY NO_PROXY
 
-# Assume an AWS IAM role and get temporary security credentials.
-# The duration of the credentials is set to 3600 seconds (1 hour).
-# The '--role-arn' parameter specifies the ARN of the IAM role to assume.
-# The '--role-session-name' parameter provides a name for the role session.
-# The '--endpoint-url' parameter specifies the AWS STS endpoint URL.
+# Assume role and get temporary security credentials.
 assumrole=$(aws sts assume-role \
   --duration-seconds 3600 \
   --role-arn arn:aws:iam::xxxxxx:role/xxxx-stg-usermng-deveks-role-001 \
   --role-session-name aws-infra-cli \
   --endpoint-url https://sts.ap-northeast-1.amazonaws.com)
 
-# Extract and store the Access Key ID from the assume-role response using jq.
+# Extract, store the Access Key ID.
 AWS_ACCESS_KEY_ID=$(jq -r '.Credentials.AccessKeyId' <<< ${assumrole})
 echo $AWS_ACCESS_KEY_ID
-
-# Export the Access Key ID as an environment variable for AWS CLI to use.
 export AWS_ACCESS_KEY_ID
 
-# Extract and store the Secret Access Key from the assume-role response using jq.
+# Extract, store the Secret Access Key.
 AWS_SECRET_ACCESS_KEY=$(jq -r '.Credentials.SecretAccessKey' <<< ${assumrole})
 echo $AWS_SECRET_ACCESS_KEY
-
-# Export the Secret Access Key as an environment variable for AWS CLI to use.
 export AWS_SECRET_ACCESS_KEY
 
-# Extract and store the Session Token from the assume-role response using jq.
+# Extract, store the Session Token.
 AWS_SESSION_TOKEN=$(jq -r '.Credentials.SessionToken' <<< ${assumrole})
 echo $AWS_SESSION_TOKEN
-
-# Export the Session Token as an environment variable for AWS CLI to use.
 export AWS_SESSION_TOKEN
 
 # Add the path to the Gloo CLI binary to the system PATH.
-# Assuming the Gloo CLI binary is located at $HOME/.gloo/bin.
 export PATH=$HOME/.gloo/bin:$PATH
 
-# Print 'Done' to the console.
+# Notify 'done'
 echo 'Done'
 
-# Start an interactive bash shell.
 bash
